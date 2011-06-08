ba410aae846c820ba3b7050f227335bbd9f23598
Paolo Perego <thesp0nge@gmail.com>
2011-06-09 00:14:19 +0200

    Version bumped to 0.10. Now the API is quite acceptable. Engine class controls the injection procedure with the injection method.
    Now it's time to build the real stuff...

--- lib/cross/engine.rb
+++ lib/cross/engine.rb
@@ -1,16 +1,36 @@
 require 'singleton'
 
 module Cross
+  # Engine is the cross class using Mechanize to inject canary and check for
+  # output
   class Engine
     include Singleton
 
     attr_reader :agent
     
-    # Start the engine
+    # Starts the engine
     def start
       @agent = Mechanize.new {|a| a.log = Logger.new("cross.log")}
       @agent.user_agent_alias = 'Mac Safari'
     end 
+
+    def inject(url)
+      found = false
+      page = @agent.get(url)
+      page.forms.each do |f|
+        f.fields.each do |ff|
+          ff.value = "<script>alert('cross canary');</script>"
+        end
+        pp = @agent.submit(f)
+        scripts = pp.search("//script")
+        scripts.each do |sc|
+          if sc.children.text == "alert('cross canary');"
+            found = true
+          end
+        end
+      end 
+      found
+    end
 
   end
 end
