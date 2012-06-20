88ee1c04f2d2d62dba28d725b26c53b92ffb4ca7
Paolo Perego <thesp0nge@gmail.com>
2012-06-20 11:45:33 +0200

    Switched from jeweler to bundle gem system

--- Gemfile
+++ Gemfile
@@ -1,19 +1,4 @@
-source "http://rubygems.org"
+source 'https://rubygems.org'
 
-gem "rest-open-uri"
-gem "mechanize"
-gem "awesome_print"
-gem 'rake'#, '0.8.7'
-# Add dependencies required to use your gem here.
-# Example:
-#   gem "activesupport", ">= 2.3.5"
-
-# Add dependencies to develop your gem here.
-# Include everything needed to run rake, tests, features, etc.
-group :development do
-  gem "rspec", "~> 2.3.0"
-  gem "yard", "~> 0.6.0"
-  gem "bundler", "~> 1.0.0"
-  gem "jeweler", "~> 1.6.0"
-  gem "rcov", ">= 0"
-end
+# Specify your gem's dependencies in cross.gemspec
+gemspec
--- lib/cross/engine.rb
+++ lib/cross/engine.rb
@@ -7,28 +7,57 @@ module Cross
     include Singleton
 
     attr_reader :agent
-    
+
     # Starts the engine
     def start
       @agent = Mechanize.new {|a| a.log = Logger.new("cross.log")}
       @agent.user_agent_alias = 'Mac Safari'
     end 
 
-    def inject(url)
+    def inject(url, options={:exploit_url=>false, :debug=>false})
       found = false
-      page = @agent.get(url)
-      page.forms.each do |f|
-        f.fields.each do |ff|
-          ff.value = "<script>alert('cross canary');</script>"
-        end
-        pp = @agent.submit(f)
-        scripts = pp.search("//script")
+      if options[:exploit_url]
+        # You ask to exploit the url, so I won't check for form values
+
+        page = @agent.get(url+"<script>alert('cross canary');</script>")
+        scripts = page.search("//script")
         scripts.each do |sc|
           if sc.children.text == "alert('cross canary');"
             found = true
           end
         end
-      end 
+
+        if options[:debug]
+          puts page.body
+        end
+
+        page = @agent.get(url+"/--><script>alert('cross canary');</script>")
+        scripts = page.search("//script")
+        scripts.each do |sc|
+          if sc.children.text == "alert('cross canary');"
+            found = true
+          end
+        end
+
+        if options[:debug]
+          puts page.body
+        end
+
+      else
+        page = @agent.get(url)
+        page.forms.each do |f|
+          f.fields.each do |ff|
+            ff.value = "<script>alert('cross canary');</script>"
+          end
+          pp = @agent.submit(f)
+          scripts = pp.search("//script")
+          scripts.each do |sc|
+            if sc.children.text == "alert('cross canary');"
+              found = true
+            end
+          end
+        end 
+      end
       found
     end
 
